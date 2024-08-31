77c5e06817fcd01af9501dba78e53d22abc9e930
netscrawler <mariarthyjamesoo@gmail.com>
2024-08-31 20:25:29 +0300

    0.0.1

---
+++ Kursach/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kursach
+{
+    abstract class Person:User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string LastName { get; set; } = null;
+        public DateOnly Birthday { get; set; }
+        public string Email { get; set; }
+
+        public abstract void Add(string Name, string LastName, string Surname, DateOnly Birthday, string Email);
+        
+
+       
+
+    }
+}
---
+++ Kursach/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kursach
+{
+    abstract class User
+    {
+        public string Login { get; set; }
+        public byte[] Password { get; set; }
+
+        public void Add(String Login, string RawPassword) {
+            this.Login = Login;
+            this.Password = GetHash(ReadBytes(RawPassword));
+            
+        }
+
+        public bool Logining(String Login, string RawPassword)
+        {
+            if (System.Security.Cryptography.SHA256.Equals(this.Password, GetHash(ReadBytes(RawPassword)))&& (this.Login == Login))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private byte[] ReadBytes(string RawPassword) { 
+            
+            return ASCIIEncoding.ASCII.GetBytes(RawPassword);
+        }
+
+        private byte[] GetHash(byte[] RawPassword) { 
+            return System.Security.Cryptography.SHA256.HashData(RawPassword);
+        }
+    }
+}
